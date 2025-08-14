2a1b31e0921aa2c1b96fd2e95c8c320e1ad8a673
Favour <favourolasoji@student.oauife.edu.ng>
2025-08-14 22:19:00 +0100

    db fix

--- Backend/app/api/routes/auth.py
+++ Backend/app/api/routes/auth.py
@@ -12,8 +12,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY') 
-ALGORITHM = os.getenv('ALGORITHM')
+SECRET_KEY = os.getenv('SECRET_KEY', "mysecret") 
+ALGORITHM = os.getenv('ALGORITHM',"HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
